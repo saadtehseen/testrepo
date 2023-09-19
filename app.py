1a0fdf7d220a92c7da15f97b18d273d5d2ff3017
saadtehseen <145502060+saadtehseen@users.noreply.github.com>
2023-09-20 02:18:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
-# Define your function
-def my_function():
-    # Your function logic here
-    return "Hello, World!"
+def func():
+    x=2
+    return x
 
-# Expose the function as a REST API endpoint
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/get_output', methods=['GET'])
-def get_output():
-    result = my_function()
-    return result
-
-if __name__ == '__main__':
-    app.run()
+func()
